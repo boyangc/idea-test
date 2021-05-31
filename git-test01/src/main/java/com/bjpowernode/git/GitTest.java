06d715423316cee22cf68a8aaadef21d8a5f3e48
cai_glb <boyang_c@zju.edu.cn>
2021-05-31 14:37:12 +0800

    idea push commit

--- git-test01/src/main/java/com/bjpowernode/git/GitTest.java
+++ git-test01/src/main/java/com/bjpowernode/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("Hello git hot-fix!");
         System.out.println("Hello git hot-fix2!");
         System.out.println("Hello git master test");
+        System.out.println("Hello git push test");
     }
 }
